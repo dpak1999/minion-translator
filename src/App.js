13345447b018dcfa59b54226c7a61a53be8c61da
dpak1999 <deepakkumardash313@gmail.com>
2021-08-20 21:34:07 +0530

    error handling

--- src/App.js
+++ src/App.js
@@ -12,9 +12,15 @@ function App() {
     e.preventDefault();
     const res = await fetch(`${URI}?text=${text}`);
     const data = await res.json();
-    if (data) {
-      setTranslatedText(data);
-      setIsTranslated(true);
+
+    try {
+      if (data) {
+        setTranslatedText(data.contents.translated);
+        setIsTranslated(true);
+      }
+    } catch (error) {
+      setText("");
+      alert(data.error.message);
     }
   };
 
@@ -23,20 +29,46 @@ function App() {
       <h1 className="text-white font-bold text-4xl text-center italic font-serif underline">
         Banana Translator
       </h1>
-
-      <div className="flex min-h-screen">
-        <form className="m-auto flex" onSubmit={handleSubmit}>
-          <input
-            className="rounded-l-lg p-4 w-96 text-gray-800 outline-none"
-            placeholder="Enter the text you want to translate"
-            onChange={(e) => setText(e.target.value)}
-            value={text}
-          />
-          <button className="px-8 rounded-r-lg bg-yellow-400  text-white font-bold p-4 uppercase">
-            Translate
-          </button>
-        </form>
-      </div>
+      {isTranslated === false ? (
+        <>
+          <div className="flex min-h-screen">
+            <form className="m-auto flex" onSubmit={handleSubmit}>
+              <input
+                className="rounded-l-lg p-4 w-96 text-gray-800 outline-none"
+                placeholder="Enter the text you want to translate"
+                onChange={(e) => setText(e.target.value)}
+                value={text}
+              />
+              <button className="px-8 rounded-r-lg bg-yellow-400  text-white font-bold p-4 uppercase">
+                Translate
+              </button>
+            </form>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="flex min-h-screen">
+            <form className="m-auto flex">
+              <input
+                className="rounded-l-lg p-4 w-96 text-white font-bold outline-none cursor-not-allowed"
+                placeholder="Enter the text you want to translate"
+                disabled
+                value={translatedText && translatedText}
+              />
+              <button
+                className="px-8 rounded-r-lg bg-yellow-400  text-white font-bold p-4 uppercase "
+                onClick={(e) => {
+                  e.preventDefault();
+                  setIsTranslated(false);
+                  setText("");
+                }}
+              >
+                Translate Again
+              </button>
+            </form>
+          </div>
+        </>
+      )}
     </div>
   );
 }
